940099bfac1293c024edfb7e26af312613b3f8bd
olacola06 <olamarchen@gmail.com>
2022-11-01 19:53:35 +0200

    01 Nov

--- src/main/java/manager/HelperCalendar.java
+++ src/main/java/manager/HelperCalendar.java
@@ -6,19 +6,20 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.text.Format;
+import java.time.LocalDate;
 
 public class HelperCalendar extends HelperBase{
     public HelperCalendar(WebDriver driver){
         super(driver);
 
     }
-
-    public void selectFirstDate(String today, String date) { //("30/10/2022","10/02/2023");
+    public void selectEventDate(String today, String date) { //("30/10/2022","10/02/2023");
         new WebDriverWait(wd,30).until(ExpectedConditions
                 .visibilityOf(wd.findElement(By.xpath("//a[.='EVENTS']"))));
         String[]startDate = date.split("/");
         String[]todayDate = today.split("/");
         chooseMonth(Integer.parseInt(startDate[1]),Integer.parseInt(todayDate[1]));
+        chooseDay(Integer.parseInt(startDate[0]));
 
     }
 
@@ -29,5 +30,23 @@ public class HelperCalendar extends HelperBase{
                 click(By.cssSelector("span[aria-label='Next Month']"));
             }
         }
+    }
+    private void chooseDay(int startDay){
+        String locator = String.format("//div[.='%s']",startDay);
+        click(By.xpath(locator));
+    }
+    public boolean getAvailableEvents(){
+        Boolean bool = wd.findElement(By.cssSelector("div[class='f35 note']")).isDisplayed();
+        if(bool) {
+            String message = wd.findElement(By.cssSelector("div[class='f35 note']")).getText();
+            System.out.println(message.toString());
+        }
+        return bool;
+    }
+
+    public void selectEventDateLD(String today, String date) {
+        //LocalDate eventDate = LocalDate.of(date);
+        LocalDate todayDate = LocalDate.now();
+
     }
 }
--- src/test/java/tests/CalendarTests.java
+++ src/test/java/tests/CalendarTests.java
@@ -1,11 +1,18 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class CalendarTests extends TestBase{
 
     @Test
     public void selectEventsDates(){
-        app.calendar().selectFirstDate("30/10/2022","10/02/2023");
+        app.calendar().selectEventDate("01/11/2022","10/05/2023");
+
+        Assert.assertTrue(app.calendar().getAvailableEvents());
+    }
+    @Test
+    public void selectEventLD(){
+        app.calendar().selectEventDateLD("01/11/2022","10/05/2023");
     }
 }
